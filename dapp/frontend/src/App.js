bbff9cb9145bab22ed73f944cf0bc653ad289658
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-06 01:31:53 -0700

    Create App.js

---
+++ dapp/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Wallet from './components/Wallet';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Wallet />
+    </div>
+  );
+}
+
+export default App;
